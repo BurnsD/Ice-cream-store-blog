519989f2237fef7f2f3c5084cd785f7905775e03
BurnsD <Dustinburnsactech@gmail.com>
2022-03-05 13:38:49 -0600

    restored functionality of application

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,6 +1,6 @@
 async function loginFormHandler(event) {
     event.preventDefault();
-  
+  console.log("hello world");
     const email = document.querySelector('#email-login').value.trim();
     const password = document.querySelector('#password-login').value.trim();
   
@@ -15,7 +15,7 @@ async function loginFormHandler(event) {
       });
   
       if (response.ok) {
-        document.location.replace('/dashboard/');
+        document.location.replace('/dashboard');
       } else {
         alert(response.statusText);
       }
@@ -42,7 +42,7 @@ async function loginFormHandler(event) {
   
       if (response.ok) {
         console.log ("INSIDE THE LOGIN.JS.")
-        document.location.replace('/dashboard/');
+        document.location.replace('/dashboard');
       } else {
         alert(response.statusText);
       }
@@ -51,4 +51,4 @@ async function loginFormHandler(event) {
   
   document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
   
-  document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
+  // document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
